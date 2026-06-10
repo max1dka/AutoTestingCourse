9e39e2ae026e5ed8145cde254d5ba09a9b4f4051
max1dka <mitrofanov.m00@icloud.com>
2026-06-10 22:23:01 +0300

    Задачка по переменным

---
+++ src/main/java/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+    String firstOut = "Ivan";
+    int secondOut = 200;
+    int thirdOut = 500;
+    String fifthOut = "Olga";
+        System.out.println(firstOut);
+        System.out.println(secondOut);
+        System.out.println(thirdOut);
+        System.out.println(fifthOut); // Работа с переменными
+
+
+    }
+}
